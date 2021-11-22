343097f8142de8ac59d45e9d1f55cf5553bb44d4
tdesero <tom.deser@googlemail.com>
2021-11-22 23:29:55 +0100

    added tabs

--- src/components/CssProperties.js
+++ src/components/CssProperties.js
@@ -6,25 +6,25 @@ import { Accordion } from "./common/Accordion";
 
 const dimensionProperties = [
   { name: "width", label: "Width", direction: "x" },
-  { name: "height", label: "Height", direction: "y" }
+  { name: "height", label: "Height", direction: "y" },
 ];
 
 const dimensionMinMaxProperties = [
   { name: "min-width", label: "Min W", direction: "x" },
   { name: "min-height", label: "Min H", direction: "y" },
   { name: "max-width", label: "Max W", direction: "x" },
-  { name: "max-height", label: "Max H", direction: "y" }
+  { name: "max-height", label: "Max H", direction: "y" },
 ];
 
 const fontProperties = [
   { name: "font-size", label: "Font Size", direction: "y" },
   { name: "line-height", label: "Line", direction: "y" },
-  { name: "letter-spacing", label: "Spacing", direction: "y" }
+  { name: "letter-spacing", label: "Spacing", direction: "y" },
 ];
 
 const textAlignProperty = {
   name: "text-align",
-  options: ["left", "center", "right"]
+  options: ["left", "center", "right"],
 };
 
 const fontFamilyProperty = { name: "font-family", label: "Font Family" };
@@ -34,35 +34,35 @@ const fontColorProperty = { name: "color", label: "Text Color" };
 const fontWeightProperty = {
   name: "font-weight",
   label: "Font Family",
-  options: ["100", "200", "300", "400", "500", "600", "700"]
+  options: ["100", "200", "300", "400", "500", "600", "700"],
 };
 
 const PaddingProperties = [
   { name: "padding-top", label: "Top", direction: "y" },
   { name: "padding-right", label: "Right", direction: "x" },
   { name: "padding-bottom", label: "Bottom", direction: "y" },
-  { name: "padding-left", label: "Left", direction: "x" }
+  { name: "padding-left", label: "Left", direction: "x" },
 ];
 
 const marginProperties = [
   { name: "margin-top", label: "Top", direction: "y" },
   { name: "margin-right", label: "Right", direction: "x" },
   { name: "margin-bottom", label: "Bottom", direction: "y" },
-  { name: "margin-left", label: "Left", direction: "x" }
+  { name: "margin-left", label: "Left", direction: "x" },
 ];
 
 const borderWidthProperties = [
   { name: "border-top-width", label: "Top", direction: "y" },
   { name: "border-right-width", label: "Right", direction: "x" },
   { name: "border-bottom-width", label: "Bottom", direction: "y" },
-  { name: "border-left-width", label: "Left", direction: "x" }
+  { name: "border-left-width", label: "Left", direction: "x" },
 ];
 
 const borderRadiusProperties = [
   { name: "border-top-left-radius", label: "Top Left" },
   { name: "border-top-right-radius", label: "Top Right" },
   { name: "border-bottom-right-radius", label: "B. Right" },
-  { name: "border-bottom-left-radius", label: "B. Left" }
+  { name: "border-bottom-left-radius", label: "B. Left" },
 ];
 
 const borderColorProperty = { name: "border-color", label: "Border Color" };
@@ -71,44 +71,50 @@ const colorProperties = [{ name: "background-color", label: "Background C." }];
 
 const positionProperty = {
   name: "position",
-  options: ["static", "relative", "absolute", "fixed", "sticky"]
+  options: ["static", "relative", "absolute", "fixed", "sticky"],
 };
 
 const positionDirectionProperties = [
   { name: "top", label: "Top", direction: "y" },
   { name: "right", label: "Right", direction: "x" },
   { name: "bottom", label: "Bottom", direction: "y" },
-  { name: "left", label: "Left", direction: "x" }
+  { name: "left", label: "Left", direction: "x" },
 ];
 
 const displayProperty = {
   name: "display",
-  options: ["flex", "inline-flex", "block", "inline-block", "inline"]
+  options: ["flex", "inline-flex", "block", "inline-block", "inline"],
 };
 
 const justifyContentProperty = {
   name: "justify-content",
-  options: ["flex-start", "center", "flex-end", "space-between", "space-around"]
+  options: [
+    "flex-start",
+    "center",
+    "flex-end",
+    "space-between",
+    "space-around",
+  ],
 };
 
 const alignItemsProperty = {
   name: "align-items",
-  options: ["flex-start", "center", "flex-end", "stretch"]
+  options: ["flex-start", "center", "flex-end", "stretch"],
 };
 
 const flexProperties = [
   {
     name: "flex-wrap",
-    options: ["no-wrap", "wrap"]
+    options: ["no-wrap", "wrap"],
   },
   {
     name: "flex-grow",
-    options: ["0", "1"]
+    options: ["0", "1"],
   },
   {
     name: "flex-shrink",
-    options: ["0", "1"]
-  }
+    options: ["0", "1"],
+  },
 ];
 
 export default function CssProperties({ cssClass }) {
@@ -209,7 +215,7 @@ export default function CssProperties({ cssClass }) {
           ))}
         </div>
       </Accordion>
-      <Accordion title="Element Alignment" isOpen={false}>
+      <Accordion title="Layout" isOpen={false}>
         <div className="mb-3">
           <CssPropertyButtonGroup
             propertyName={displayProperty.name}
--- src/components/InspectorPanel.js
+++ src/components/InspectorPanel.js
@@ -9,6 +9,8 @@ import saveJSON from "./../helpers/saveJSON";
 import ElementInfos from "./ElementInfos";
 import CssEditor from "./CssEditor";
 
+import { Tab } from "@headlessui/react";
+
 export const SelectClassContext = createContext();
 
 export default function InspectorPanel() {
@@ -32,7 +34,7 @@ export default function InspectorPanel() {
     currentActive,
     getClassByName,
     importJSON,
-    changeElementSrc
+    changeElementSrc,
   } = useContext(Context);
 
   useEffect(() => {
@@ -65,90 +67,120 @@ export default function InspectorPanel() {
   return (
     <SelectClassContext.Provider value={selectClass}>
       <div className="w-96 flex-shrink-0 border-l shadow-xl overflow-auto bg-white">
-        {
-          //we don't need this, if it's a textnode
-          currentActive && currentActive.tag && (
-            <CssEditor
-              {...{
-                elementSelected: currentActive,
-                addClassName,
-                cssClass,
-                classNames,
-                selectClass,
-                setPseudo,
-                pseudo,
-                selectPseudo,
-                val,
-                setVal,
-                updatePseudoClass,
-                updateClassText,
-                removeClassFromElement
-              }}
-            />
-          )
-        }
-        <Accordion title="Element Details" isOpen={false}>
-          <ElementInfos
-            {...{
-              elementSelected: currentActive,
-              text,
-              setText,
-              changeElementText,
-              selectClass,
-              classNames,
-              addClassName,
-              removeClassFromElement,
-              newClassName,
-              setNewClassName,
-              addClassToElement,
-              changeElementTag,
-              changeElementSrc
-            }}
-          />
-        </Accordion>
-        <Accordion title="All CSS Classes" isOpen={false}>
-          {classNames.map((n) => (
-            <BadgeButton
-              key={n.name}
-              isActive={n.name === cssClass?.name ? true : false}
-              onClick={() => selectClass(n)}
-              onDelete={() => removeClassName(n)}
+        <Tab.Group>
+          <Tab.List className="border-b flex text-xs tracking-wider">
+            <Tab
+              className={({ selected }) =>
+                selected
+                  ? "p-4 border-b-2 border-indigo-500 flex-1 font-bold uppercase"
+                  : "p-4 flex-1 font-bold uppercase"
+              }
             >
-              <button
-                className="mr-2"
-                onClick={() => addClassToElement(currentActive.id, n.name)}
-              >
-                +
-              </button>
-              {n.name}
-            </BadgeButton>
-          ))}
-        </Accordion>
-        <Accordion title="Export" isOpen={false}>
-          <button
-            className="mt-3 w-full py-2 px-6 rounded border text-indigo-500 border-indigo-500 flex-grow"
-            onClick={() => exportHTML({ classNames, elements })}
-          >
-            Export HTML
-          </button>
-          <button
-            className="mt-3 w-full py-2 px-6 rounded border text-indigo-500 border-indigo-500 flex-grow"
-            onClick={() => saveJSON({ classNames, elements })}
-          >
-            Save JSON
-          </button>
-        </Accordion>
-        <Accordion title="Import" isOpen={false}>
-          <label className="mt-3 block text-center py-2 px-6 rounded border text-indigo-500 border-indigo-500 flex-grow">
-            import JSON
-            <input
-              className="hidden"
-              type="file"
-              onChange={importJSON}
-              accept="application/json"
-            />
-          </label>
-        </Accordion>
+              Element
+            </Tab>
+            <Tab
+              className={({ selected }) =>
+                selected
+                  ? "p-4 border-b-2 border-indigo-500 flex-1 font-bold uppercase"
+                  : "p-4 flex-1 font-bold uppercase"
+              }
+            >
+              Document
+            </Tab>
+          </Tab.List>
+          <Tab.Panels>
+            <Tab.Panel>
+              {
+                //we don't need this, if it's a textnode
+                currentActive && currentActive.tag && (
+                  <CssEditor
+                    {...{
+                      elementSelected: currentActive,
+                      addClassName,
+                      cssClass,
+                      classNames,
+                      selectClass,
+                      setPseudo,
+                      pseudo,
+                      selectPseudo,
+                      val,
+                      setVal,
+                      updatePseudoClass,
+                      updateClassText,
+                      removeClassFromElement,
+                    }}
+                  />
+                )
+              }
+              <Accordion title="Element Details" isOpen={false}>
+                <ElementInfos
+                  {...{
+                    elementSelected: currentActive,
+                    text,
+                    setText,
+                    changeElementText,
+                    selectClass,
+                    classNames,
+                    addClassName,
+                    removeClassFromElement,
+                    newClassName,
+                    setNewClassName,
+                    addClassToElement,
+                    changeElementTag,
+                    changeElementSrc,
+                  }}
+                />
+              </Accordion>
+            </Tab.Panel>
+            <Tab.Panel>
+              <Accordion title="All CSS Classes" isOpen={false}>
+                {classNames.map((n) => (
+                  <BadgeButton
+                    key={n.name}
+                    isActive={n.name === cssClass?.name ? true : false}
+                    onClick={() => selectClass(n)}
+                    onDelete={() => removeClassName(n)}
+                  >
+                    <button
+                      className="mr-2"
+                      onClick={() =>
+                        addClassToElement(currentActive.id, n.name)
+                      }
+                    >
+                      +
+                    </button>
+                    {n.name}
+                  </BadgeButton>
+                ))}
+              </Accordion>
+              <Accordion title="Export" isOpen={true}>
+                <button
+                  className="mt-3 w-full py-2 px-6 rounded border text-indigo-500 border-indigo-500 flex-grow"
+                  onClick={() => exportHTML({ classNames, elements })}
+                >
+                  Export HTML
+                </button>
+                <button
+                  className="mt-3 w-full py-2 px-6 rounded border text-indigo-500 border-indigo-500 flex-grow"
+                  onClick={() => saveJSON({ classNames, elements })}
+                >
+                  Save JSON
+                </button>
+              </Accordion>
+              <Accordion title="Import" isOpen={true}>
+                <label className="mt-3 block text-center py-2 px-6 rounded border text-indigo-500 border-indigo-500 flex-grow">
+                  import JSON
+                  <input
+                    className="hidden"
+                    type="file"
+                    onChange={importJSON}
+                    accept="application/json"
+                  />
+                </label>
+              </Accordion>
+            </Tab.Panel>
+          </Tab.Panels>
+        </Tab.Group>
       </div>
     </SelectClassContext.Provider>
   );
